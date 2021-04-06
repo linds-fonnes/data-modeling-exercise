cc2e782464722938e00572634479d24be5dd9089
Lindsey <linds.fonnes@gmail.com>
2021-04-06 11:56:04 -0600

    music.sql completed

--- music.sql
+++ music.sql
@@ -7,27 +7,57 @@ CREATE DATABASE music;
 
 \c music
 
+CREATE TABLE producers (
+  id SERIAL PRIMARY KEY,
+  first_name TEXT NOT NULL,
+  last_name TEXT
+);
+
+CREATE TABLE artists (
+  id SERIAL PRIMARY KEY,
+  first_name TEXT NOT NULL,
+  last_name TEXT,
+  birthdate DATE
+);
+
+
 CREATE TABLE songs
 (
   id SERIAL PRIMARY KEY,
   title TEXT NOT NULL,
   duration_in_seconds INTEGER NOT NULL,
   release_date DATE NOT NULL,
-  artists TEXT[] NOT NULL,
-  album TEXT NOT NULL,
-  producers TEXT[] NOT NULL
+  album TEXT NOT NULL
 );
 
-INSERT INTO songs
-  (title, duration_in_seconds, release_date, artists, album, producers)
-VALUES
-  ('MMMBop', 238, '04-15-1997', '{"Hanson"}', 'Middle of Nowhere', '{"Dust Brothers", "Stephen Lironi"}'),
-  ('Bohemian Rhapsody', 355, '10-31-1975', '{"Queen"}', 'A Night at the Opera', '{"Roy Thomas Baker"}'),
-  ('One Sweet Day', 282, '11-14-1995', '{"Mariah Cary", "Boyz II Men"}', 'Daydream', '{"Walter Afanasieff"}'),
-  ('Shallow', 216, '09-27-2018', '{"Lady Gaga", "Bradley Cooper"}', 'A Star Is Born', '{"Benjamin Rice"}'),
-  ('How You Remind Me', 223, '08-21-2001', '{"Nickelback"}', 'Silver Side Up', '{"Rick Parashar"}'),
-  ('New York State of Mind', 276, '10-20-2009', '{"Jay Z", "Alicia Keys"}', 'The Blueprint 3', '{"Al Shux"}'),
-  ('Dark Horse', 215, '12-17-2013', '{"Katy Perry", "Juicy J"}', 'Prism', '{"Max Martin", "Cirkut"}'),
-  ('Moves Like Jagger', 201, '06-21-2011', '{"Maroon 5", "Christina Aguilera"}', 'Hands All Over', '{"Shellback", "Benny Blanco"}'),
-  ('Complicated', 244, '05-14-2002', '{"Avril Lavigne"}', 'Let Go', '{"The Matrix"}'),
-  ('Say My Name', 240, '11-07-1999', '{"Destiny''s Child"}', 'The Writing''s on the Wall', '{"Darkchild"}');
+CREATE TABLE performances (
+  id SERIAL PRIMARY KEY,
+  song_id INTEGER REFERENCES songs ON DELETE CASCADE,
+  venue TEXT NOT NULL,
+  performance_date DATE NOT NULL
+);
+
+CREATE TABLE productions (
+  id SERIAL PRIMARY KEY,
+  producer_id INTEGER REFERENCES producers ON DELETE CASCADE,
+  song_id INTEGER REFERENCES songs ON DELETE CASCADE,
+  label TEXT NOT NULL
+);
+
+INSERT INTO producers(first_name)
+VALUES ('Darkchild'),('The Matrix'),('Shellback');
+INSERT INTO producers (first_name, last_name)
+VALUES ('Benny','Blanco');
+
+INSERT INTO artists(first_name,last_name, birthdate)
+VALUES('Avril','Lavigne','1984-09-27');
+INSERT INTO artists(first_name)
+VALUES('Destiny''s Child'),('Maroon 5');
+
+INSERT INTO songs(title, duration_in_seconds,release_date, album)
+VALUES ('Say My Name', 240, '11-07-1999','The Writing''s on the Wall'),('Complicated',244,'05-14-2002', 'Let Go'), ('Moves Like Jagger', 201, '06-22-2011', 'Hands All Over');
+
+INSERT INTO performances (song_id, venue, performance_date)
+VALUES(1, 'Staples Arena','2001-08-08'),(2,'Big Venue','2005-09-09');
+
+INSERT INTO productions (producer_id, song_id, label) VALUES (3, 3, 'A&M Octone'),(1,1,'Columbia');
